319a9cda9a6afcd5f7955f78c3aacc290481f21d
Holyman713 <54744956+Holyman713@users.noreply.github.com>
2020-12-20 21:25:49 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,107 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+
+
+
+
+/* Галлерея */
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+	});
+});
+
+
+
+
+
+// Заказ
+
+
+const addCart = document.querySelector('.addCart'),
+receipt = document.querySelector('.receipt'),
+receiptWindow = document.querySelector('.receipt__window')
+receiptWindowBtn = document.querySelector('.receipt__window-btn')
+receiptWindowExit = document.querySelector('.icon-exit')
+
+addCart.addEventListener('click', function () {
+
+
+    receipt.style.display = 'flex';
+    setTimeout(function () {
+        receipt.style.opacity = '1'
+    }, 10);
+    setTimeout(function () {
+        receiptWindow.style.top = '0';
+    }, 10);
+});
+
+
+receiptWindowBtn.addEventListener('click', function () {
+    location.reload();
+})
+receiptWindowExit.addEventListener('click', function () {
+    location.reload();
+})
+
+
+
+// меню
+
+var btn = document.querySelector(".nav__menu"),
+    menu = document.querySelector(".nav__list");
+btn.addEventListener("click", function () {
+    menu.classList.add("active")
+}),
+document.addEventListener("click", function (e) {
+    menu.classList.contains("active") && e.target != menu && "nav__list-link" != e.target.className && e.target != btn && menu.classList.remove("active")
+}); 
